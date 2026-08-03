0a6c9d0e3ec4aed984647ea2be43c104e9e95edc
Aakash Kumar <kumaraakash77396@gmail.com>
2026-08-03 20:01:13 +0530

    Initial React + Tailwind setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import React from 'react'
+import AppRoutes from './routes/AppRoutes'
 
 function App() {
-  return (
-    <div>
-      <h1>App</h1>
-    </div>
-  )
+  return <AppRoutes/>
 }
 
 export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <>
+      <h1 className="text-5xl text-center mt-20">
+        GLEAMINCG Interior Designing
+      </h1>
+    </>
+  );
+};
+
+export default Home;
