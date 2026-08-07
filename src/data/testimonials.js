fb1b796194db17eafd24f92e2f18c0ce3618dfc1
Aakash Kumar <kumaraakash77396@gmail.com>
2026-08-07 22:55:58 +0530

    testomonial

--- src/data/testimonials.js
+++ src/data/testimonials.js
@@ -2,26 +2,18 @@ export const testimonials = [
 
     {
         id: 1,
-
         name: "Amit Kumar",
-
         location: "Noida",
-
         rating: 5,
-
         review:
             "Gleamincg completely transformed our home into a luxurious and comfortable living space."
     },
 
     {
         id: 2,
-
         name: "Rahul Singh",
-
         location: "Delhi",
-
         rating: 5,
-
         review:
             "Professional team with excellent execution and timely delivery."
     }
