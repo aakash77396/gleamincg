5888df0fcf16c84496c13ee2c04aa80a3d4fe3b4
Aakash Kumar <kumaraakash77396@gmail.com>
2026-08-21 16:25:50 +0530

    protected route

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,20 +1,29 @@
-import { BrowserRouter,Routes,Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Projects from "../pages/Projects";
 import Careers from "../pages/Careers";
 import AdminDashboard from "../admin/AdminDashboard";
 import AdminLogin from "../admin/AdminLogin";
+import ProtectedRoute from "../admin/ProtectedRoute";
 
-const AppRoutes = ( )=>{
+const AppRoutes = () => {
     return (
         <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<Home/>} />
-            <Route path="/projects" element={<Projects/>} />
-            <Route path="/careers" element={<Careers/>} />
-            <Route path="/admin/dashboard" element={<AdminDashboard/>} />
-            <Route path="/admin/login" element={<AdminLogin/>} />
-        </Routes>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/projects" element={<Projects />} />
+                <Route path="/careers" element={<Careers />} />
+
+                {/* Protected Admin Routes */}
+                <Route element={<ProtectedRoute />}>
+                    <Route
+                        path="/admin/dashboard"
+                        element={<AdminDashboard />}
+                    />
+                </Route>
+
+                <Route path="/admin/login" element={<AdminLogin />} />
+            </Routes>
         </BrowserRouter>
     )
 }
